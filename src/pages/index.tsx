0582044ad1df9c16ad5d6e6849080171ab62acca
PhilNorfleet <norfleet.phillip@gmail.com>
2022-05-16 00:45:02 -0400

    means like request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next'
+import { useForm } from "react-hook-form";
 import styled from 'styled-components';
 import useSWR from 'swr';
 import axios, { AxiosResponse } from 'axios'
+import { useState } from 'react';
+import { Word } from '../common/types/Word';
 
 const fetcher = (url: string): Promise<AxiosResponse["data"]> => axios.get(url).then(res => res.data)
 
@@ -9,13 +12,20 @@ const Header = styled.h1`
   color: blue;
 `
 const Home: NextPage = () => {
-  const { data, error } = useSWR('/api/means-like?word=muffin', fetcher)
+  const { register, handleSubmit } = useForm();
+  const [ meansLikeWord, setMeansLikeWord ] = useState([]);
+  const { data, error } = useSWR(`/api/means-like?word=${meansLikeWord}`, fetcher)
   if (error) return <>error</>
   if (!data) return <>loading</>
   return (
     <>
       <Header>Backronym Helper!</Header>
-      {data.map((word: string) => `${word.word}`)}
+      <form onSubmit={handleSubmit((inputs) => setMeansLikeWord(inputs.meansLike))}>
+        <input {...register("meansLike")} placeholder="Words with a meaning similar to"/>
+      </form>
+      <div>
+        {data.map((word: Word) => <div>{word.word}</div>)}
+      </div>
     </>
   )
 }
